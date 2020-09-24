32ba7559c08372dc4549ada799dec2dd115de336
jeffzo <jeffzo01@qq.com>
2020-09-24 20:30:20 +0800

    minor modification

--- exusiai_bot/telegram_bot_utils.py
+++ exusiai_bot/telegram_bot_utils.py
@@ -1,15 +1,22 @@
 from contextlib import contextmanager
-from threading import Timer
-from typing import Optional, Union
-from telegram import Bot
 from functools import partial
+from threading import Timer
+from typing import Union
+
+from telegram import Bot
 
 
-def send_timed_message(bot: Bot, chat_id: Union[int, str], text: str,
-                       timeout: int=5000):
+def send_timed_message(
+    bot: Bot,
+    chat_id: Union[int, str],
+    text: str,
+    timeout: int = 5000,
+) -> dict:
     msg = bot.send_message(chat_id=chat_id, text=text)
     del_msg = partial(bot.delete_message,
                       chat_id=chat_id,
                       message_id=msg.message_id)
     timer = Timer(timeout / 1000, del_msg)
     timer.start()
+
+    return {"timer": timer, "message": msg}
