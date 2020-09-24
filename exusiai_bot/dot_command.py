adf5027ce92f9230d0ceeb0001ff5a158f621838
jeffzo <jeffzo01@qq.com>
2020-09-24 12:42:47 +0800

    command handler update

--- exusiai_bot/dot_command.py
+++ exusiai_bot/dot_command.py
@@ -35,15 +35,14 @@ class DotCommandDispatcher:
             command, args_string = context.matches[0].groups()
             print(f"received dot command: {command} {args_string}")
             # print(f"{self._commands=}")
-            try:
-                self._commands[command]
-            except KeyError as e:
-                # print(f"keyError {e=} {dir(e)=}")
-                print(f"argv: {command=}, {args_string=}")
-                self._default(update, context, argv=(command, args_string))
-            else:
+            command_handler = self._commands.get(command)
+            if command_handler:
                 self._commands[command](update, context,
                                         argv=(command, args_string))
+            else:
+                print(f"argv: {command=}, {args_string=}")
+                self._default(update, context, argv=(command, args_string))
+
 
         self._dot_command_pattern = r"(?:^[\.。](?P<command>[\u4e00-\u9fa5a-zA-Z0-9]+)\s?)(?:(?P<args>.*)\s*)"
         dispatcher.add_handler(MessageHandler(Filters.regex(self._dot_command_pattern),
