03190351567b094e1ed75416140bc1371c592564
jeffzo <jeffzo01@qq.com>
2020-09-25 15:47:25 +0800

    regex fix

--- exusiai_bot/dice_commands.py
+++ exusiai_bot/dice_commands.py
@@ -101,7 +101,7 @@ def dot_command_filter(
     command, args_string = argv
     match = re.match(r"(?:r(.+))", command)
     logging.info(f"filter received command: {command} {args_string}, {match=}")
-    if match and Dice.test_dice_code(match.string):
+    if match and Dice.test_dice_code(match.group):
         return update, context, ("r", f"{match.string} {args_string}")
     return update, context, argv
 
