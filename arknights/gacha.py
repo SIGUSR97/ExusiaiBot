addaf8424e5c8fb7f559b33de11f388eebc076ba
jeffzo <jeffzo01@qq.com>
2020-09-28 10:58:28 +0800

    arknights banner

--- arknights/gacha.py
+++ arknights/gacha.py
@@ -1,6 +1,18 @@
-class Gacha:
-    pass
+from abc import ABC, abstractmethod
 
 
-class Banner:
-    pass
+class GachaBanner(ABC):
+    @abstractmethod
+    def __init__(self) -> None:
+        pass
+
+
+class ArknightsBanner(GachaBanner):
+    SIX_STAR_RATE = 0.02
+    FIVE_STAR_RATE = 0.10
+    def __init__(
+        self,
+        name: str,
+        rateups: dict,
+    ) -> None:
+        pass
